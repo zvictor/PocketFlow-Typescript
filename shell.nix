68dca828fbdf6e4dfde0e79fa21561acaa676d9b
zvictor <zvictor@users.noreply.github.com>
2025-03-27 19:56:49 +0100

    setup python environment

--- shell.nix
+++ shell.nix
@@ -1,10 +1,38 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  venvDir = "./.venv";
+in pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     ncurses
     openssh
     git
     corepack_22
     nodejs_22
+    uv
   ];
+
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.pytest
+    ]))
+  ];
+
+  shellHook = ''
+    if [ ! -d "${venvDir}" ]; then
+      echo "Installing python dependencies"
+
+      printf "Creating virtual environment...\n"
+      uv venv --python 3.12 ${venvDir}
+      source "${venvDir}/bin/activate"
+
+      if [ -f "requirements.txt" ]; then
+        printf "Installing dependencies...\n"
+        uv pip install -r requirements.txt
+      fi
+    fi
+
+    source "${venvDir}/bin/activate"
+    export PYTHONPATH="$PWD:$PYTHONPATH"
+  '';
 }
